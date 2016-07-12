500f0a0d81ab30f5ab88388dd54bf48ffab512a4
David Honour <david@foolswood.co.uk>
2016-07-12 07:46:08 +0000

    Add test to check example loading

--- questioneer/__main__.py
+++ questioneer/__main__.py
@@ -1,21 +1,9 @@
 from sys import argv
 
-import yaml
-
 from .server import run
-from .metric import from_description
-from .survey_types import InterRater
 from .persistence import InMemory
-
-
-def survey_from_config(config, storage):
-    metric = from_description(config['metric'])
-    return InterRater(
-        config['title'], config['description'], metric, argv[1] + 'items',
-        storage)
+from .plumbing import survey_from_path
 
 
 storage = InMemory()
-with open(argv[1] + 'metric.yaml') as f:
-    cfg = yaml.load(f)
-run(survey_from_config(cfg, storage))
+run(survey_from_path(argv[1], storage))
---
+++ test/test_examples_load.py
@@ -0,0 +1,32 @@
+from asyncio import get_event_loop
+from os.path import dirname, join
+from os import listdir, pardir
+from html.parser import HTMLParser
+
+import pytest
+
+from questioneer.plumbing import survey_from_path
+from questioneer.persistence import InMemory
+
+_module_base = join(dirname(__file__), pardir)
+_examples_path = join(_module_base, 'examples')
+
+loop = get_event_loop()
+
+
+def _check_valid_html(text):
+    p = HTMLParser()
+    p.feed(text)
+    p.close()
+
+
+@pytest.mark.parametrize(
+    'example_path',
+    (join(_examples_path, e) for e in listdir(_examples_path)))
+def test_example_loads(example_path):
+    storage = InMemory()
+    survey = survey_from_path(example_path, storage)
+    index_text = loop.run_until_complete(survey.get_index(lambda _: ''))
+    _check_valid_html(index_text)
+    item_text = loop.run_until_complete(survey.get_item('someone'))
+    _check_valid_html(item_text)
